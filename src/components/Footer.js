96749d26dcd9d5843d2fdfb767b4b7f4d69e62d4
Shubham Badwial <shubhambadiwal8520@gmail.com>
2024-03-01 12:13:11 +0530

    Update Components.css and Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@
 import React from 'react'
 import "./Components.css"
 import Image from "next/image";
-import wordpresslogo from "../../public/wordpresslogo.png"
+import logo from "../../public/logo.webp"
 
 
 const Footer = () => {
@@ -17,36 +17,75 @@ const Footer = () => {
                             Gravitate Towards Unparalleled WordPress Excellence
                         </h1>
                     </div>
-                    <div className="grid item-center gap-2 lg:grid-cols-2">
-                        <div>
-                            <div className="relative lg:ml-9 item-left grid grid-rows-1 gap-0 lg:px-12 md:px-8">
 
-                                <div className=' relative item-left grid grid-rows-6 gap-5'>
-                                    <div >
-                                        <p className=" font-bold text-blue-500  ">
-                                            --- Wordpress Plugin Development
-                                        </p>
-                                    </div>
-                                    <div >
-                                        <p className=" text-blue font-bold ">
-                                            Wordpress CMS Development
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <p className="font-bold ">
-                                            User Testing & Feedback
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <p className=" font-bold">
-                                            Training and Support
-                                        </p>
-                                    </div>
-                                    <div >
-                                        <p className=" font-bold">
-                                            Performance Monitoring and optimization
-                                        </p>
-                                    </div>
+
+                    <div className="grid  lg:grid-cols-2">
+                        <div>
+                            <div className=" lg:ml-9 text-center grid grid-rows-1 gap-3 lg:px-12 md:px-8">
+                                <div >
+                                    <Image
+                                        src={logo}
+                                        alt="Logo"
+                                        width={171}
+                                        height={115}
+                                    ></Image>
+                                </div>
+                                <div className='text-left' >
+                                    <h1
+                                        className="text-3xl text-white  font-semithin tracking-tight md:text-2xl xl:text-2xl">
+                                        let's build
+                                    </h1>
+                                    <h1
+                                        className="text-3xl text-white  font-semibold tracking-tight md:text-2xl xl:text-3xl">
+                                        something awesome
+                                    </h1>
+                                </div>
+                                <div>
+                                    <p className="font text-white text-left">
+                                        #503, 5th Floor, Hari Alokik Heights, Subhash Nagar, near Ahinsa Circle, Panch Batti, C Scheme, Ashok Nagar, Jaipur, Rajasthan 302001
+                                    </p>
+                                </div>
+                                <div>
+                                    <p className=" font-semibold text-white text-left">
+                                        sales@webnautical.com
+                                    </p>
+                                </div>
+                                <div >
+                                    <p className=" font-semibold text-white text-left">
+                                       +91 9530488844
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div className=" lg:ml-9 item-left grid grid-rows-1 gap-3 lg:px-12 md:px-8">
+                                <div >
+                                    <Image
+                                        src={logo}
+                                        alt="Logo"
+                                        width={171}
+                                        height={115}
+                                    ></Image>
+                                </div>
+                                <div >
+                                    <p className=" text-blue font-bold ">
+                                        Wordpress CMS Development
+                                    </p>
+                                </div>
+                                <div>
+                                    <p className="font-bold ">
+                                        User Testing & Feedback
+                                    </p>
+                                </div>
+                                <div>
+                                    <p className=" font-bold">
+                                        Training and Support
+                                    </p>
+                                </div>
+                                <div >
+                                    <p className=" font-bold">
+                                        Performance Monitoring and optimization
+                                    </p>
                                 </div>
                             </div>
                         </div>
